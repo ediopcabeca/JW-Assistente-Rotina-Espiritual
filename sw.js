8f8e5188a1a9241b3220177a8cbca64cd8af0cb6
imaculadagoliver-cmyk <imaculadagoliver@gmail.com>
2026-01-27 19:15:22 -0300

    Fix: Disable Service Worker and force cache clear

--- sw.js
+++ sw.js
@@ -1,31 +1,15 @@
-const CACHE_NAME = 'jw-assistente-v2';
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Force new SW to activate immediately
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/', '/index.html']);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
+        return caches.delete(key);
       }));
-    })
-  );
-  return self.clients.claim(); // Become available to all pages
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    }).then(() => {
+      return self.registration.unregister();
     })
   );
 });
