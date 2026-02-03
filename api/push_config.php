3445bfa9247433b3605ddab58b172a8393f7a781
imaculadagoliver-cmyk <imaculadagoliver@gmail.com>
2026-02-03 12:16:14 -0300

    Fix: v1.8.0 Chaves VAPID Estáticas e Estáveis (Final)

--- api/push_config.php
+++ api/push_config.php
@@ -1,18 +1,9 @@
 <?php
-// api/push_config.php - v1.6.2
-// Chaves VAPID reais geradas para o protocolo Web Push (P-256)
-$VAPID_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgs9QXcG4VcDQ9XeeE
-YVeCshR/5nZBfXUrUYxALWMu+eKhRANCAARS/g8cS56lqmb+xY1a04jLmGKg0P2U
-Lnhv5up3qSjhEm1jnLLda1UudWNBCUrbpGmhlDmRtAHsbYBPeRWApoN2
------END PRIVATE KEY-----";
+// api/push_config.php - v1.8.0
+// Chaves VAPID Estáticas e Estáveis
 
-// Extração automática da chave pública P-256
-$res = openssl_pkey_get_private($VAPID_PRIVATE_KEY);
-$details = openssl_pkey_get_details($res);
-$pubKeyBinary = $details['ec']['public_key'];
-$VAPID_PUBLIC_KEY = rtrim(strtr(base64_encode($pubKeyBinary), '+/', '-_'), '=');
-
+$VAPID_PUBLIC_KEY = 'BG4VYkdzZ9ueo3Ry_8bomwBu_7iQ3WsVMzkaYkf91hVd5FZZj6Hoi2dwua92vdQbOd_9twskmZ4-E5HYIvnvPwA';
+$VAPID_PRIVATE_KEY = 'o6ZqmL5hvCPOyXFnnvHz54K5p-NnUis5sPqMZtXfAq4';
 $VAPID_SUBJECT = 'mailto:ediopereira1978@hotmail.com';
 
 if (basename($_SERVER['PHP_SELF']) == 'push_config.php') {
