7ec192d3dd5547fcc1f83f257aa953500df9c8f0
imaculadagoliver-cmyk <imaculadagoliver@gmail.com>
2026-02-03 11:46:32 -0300

    Fix: v1.7.9 Extração automática de Public Key VAPID no servidor

--- dist/api/push_config.php
+++ dist/api/push_config.php
@@ -1,12 +1,18 @@
 <?php
 // api/push_config.php - v1.6.2
 // Chaves VAPID reais geradas para o protocolo Web Push (P-256)
-$VAPID_PUBLIC_KEY = 'RS-g8cS56lqmb-xY1a04jLmGKg0P2ULnhv5up3qSjhEm1jnLLda1UudWNBCUrbpGmhlDmRtAHsbYBPeRWApoN2';
-$VAPID_PRIVATE_KEY = '-----BEGIN PRIVATE KEY-----
+$VAPID_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----
 MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgs9QXcG4VcDQ9XeeE
 YVeCshR/5nZBfXUrUYxALWMu+eKhRANCAARS/g8cS56lqmb+xY1a04jLmGKg0P2U
 Lnhv5up3qSjhEm1jnLLda1UudWNBCUrbpGmhlDmRtAHsbYBPeRWApoN2
------END PRIVATE KEY-----';
+-----END PRIVATE KEY-----";
+
+// Extração automática da chave pública P-256
+$res = openssl_pkey_get_private($VAPID_PRIVATE_KEY);
+$details = openssl_pkey_get_details($res);
+$pubKeyBinary = $details['ec']['public_key'];
+$VAPID_PUBLIC_KEY = rtrim(strtr(base64_encode($pubKeyBinary), '+/', '-_'), '=');
+
 $VAPID_SUBJECT = 'mailto:ediopereira1978@hotmail.com';
 
 if (basename($_SERVER['PHP_SELF']) == 'push_config.php') {
